34bf9918c76e6d9127f1dd50264105f08ec81ca0
Aditya Agarwal <adityag2511@gmail.com>
2024-05-04 15:35:50 -0700

    [NewRoute] Added /bot route to act as webhook endpoint for Telegram

--- api/index.py
+++ api/index.py
@@ -1,6 +1,14 @@
 from flask import Flask
+import telebot
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+TELEGRAM_TOKEN = os.getenv('NEXT_TELEGRAM_TOKEN')
 
 app = Flask(__name__)
+bot = telebot.TeleBot(token=TELEGRAM_TOKEN, threaded=False)
 
 @app.route('/')
 def home():
@@ -9,3 +17,7 @@ def home():
 @app.route('/about')
 def about():
     return 'About'
+
+@app.route('/bot')
+def bot():
+    return 'Bot'
