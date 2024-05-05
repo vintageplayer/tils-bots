64a55c0896611fd772b9621db60225a03f4c0a2a
Aditya Agarwal <adityag2511@gmail.com>
2024-05-04 18:08:29 -0700

    [TG_BOT] Printing fields of chat messages to store

--- api/index.py
+++ api/index.py
@@ -12,12 +12,21 @@ app = Flask(__name__)
 bot = telebot.TeleBot(token=TELEGRAM_TOKEN, threaded=False)
 configure_routes(app, bot)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['help'])
 def command_start(message):
-    bot.reply_to(message, 
-        ("Hi there, I am EchoBot.\n"
-        "I am here to echo your kind words back to you"))
+    bot.reply_to(message,
+        ("A tool to help you capture your notes quickly:\n",
+         "Later Analyze and link the notes"))
+
+@bot.message_handler(commands=['start'])
+def command_start(message):
+    bot.reply_to(message, f"Please Enter the Thoughts ({message.message_id}, {message.message_thread_id}, {message.date}, {message.from_user.id}, {message.from_user.first_name}, {message.from_user.last_name}, {message.from_user.username}):\n")
+
+@bot.message_handler(commands=['end'])
+def command_start(message):
+    bot.reply_to(message, "Notes Stored\n")
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_to(message, message.text)
+    pass
+    bot.reply_to(message, f"Please Enter the Thoughts ({message.message_id}, {message.message_thread_id}, {message.text}):\n")
